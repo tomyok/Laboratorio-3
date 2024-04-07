724218cf063e6e7f4df6316da6522583b9b0bad0
Tomas Joaquin Caceres <130007103+tomyok@users.noreply.github.com>
2024-04-07 00:50:26 -0300

    Update Normalizacion1.1.sql

--- Normalizacion1.1.sql
+++ Normalizacion1.1.sql
@@ -1,25 +1,50 @@
+CREATE TABLE Cursos(
+	ID int not null primary key identity(1,1),
+	Nombre varchar(50) not null,
+)
 CREATE TABLE Costos(
 	ID int not null primary key identity(1,1),
+	IDCurso int not null foreign key references Cursos(ID),
 	CostoCursado money not null,
 	CostoCertificacion Money not null
 )
-CREATE TABLE Idioma(
+CREATE TABLE IdiomaAudio(
 	ID int not null primary key identity(1,1),
-	IdiomaAudio varchar(50) not null unique,
-	IdiomaSub varchar(50) unique
+	IDCurso int not null foreign key references Cursos(ID),
+	IdiomaAudio varchar(50) not null
+)
+CREATE TABLE Curso_x_IdiomaAudio(
+	
+)
+CREATE TABLE IdiomaSub(
+	ID int not null primary key identity(1,1),
+	IDCurso int not null foreign key references Cursos(ID),
+	IdiomaSub varchar(50) not null
 )
 CREATE TABLE FechaDeEstreno(
 	ID int not null primary key identity(1,1),
+	IDCurso int not null foreign key references Cursos(ID),
 	FechaDeEstreno date not null
 )
 CREATE TABLE Nivel(
 	ID int not null primary key identity(1,1),
+	IDCurso int not null foreign key references Cursos(ID),
 	Nivel varchar(50)
 )
-CREATE TABLE Cursos(
+
+/*--------------------------------------------------------------------*/
+
+CREATE TABLE Categorias(
 	ID int not null primary key identity(1,1),
-	Nombre varchar(50) not null
+	Nombre varchar(50) not null,
 )
+
+CREATE TABLE Categorias_x_Cursos(
+	IDCategoria int not null foreign key references Categorias(ID),
+	IDCurso int not null foreign key references Cursos(ID),
+	constraint PK_Categorias_Cursos primary key (IDCategoria, IDCurso)
+)
+/*Hacer una tabla con idcat y idcurso*/
 
 /*--------------------------------------------------------------------*/
 
