aa4776ab39f9cb68e406be1de62904edf5a49972
Tomas Joaquin Caceres <130007103+tomyok@users.noreply.github.com>
2024-07-02 23:49:36 -0300

    Finish Actividad3.1.sql
    Puntos restantes.

--- Actividad3.1.sql
+++ Actividad3.1.sql
@@ -120,9 +120,119 @@ Select
 
 	From Datos_Personales DP;
 
-	Select * From VW_Usuarios 
+Select * From VW_Usuarios 
+
+-- 6.Hacer uso de la vista creada anteriormente para obtener la cantidad de usuarios que adeuden más de los que pagaron.
+
+Select Count(*) As CantUsuariosAdeudan From VW_Usuarios V Where V.TotalAdeudado > V.TotalPagado
+
+-- 7.Hacer un procedimiento almacenado que permita dar de alta un nuevo curso. Debe recibir todos los valores necesarios para poder insertar un nuevo registro.
+
+Create Or Alter Procedure  SP_NuevoCurso(
+ @IDNivel TinyInt,
+ @Nombre Varchar(100),
+ @CostoCurso Money,
+ @CostoCertificacion Money,
+ @Estreno Date,
+ @DebeSerMayorDeEdad Bit
+)
+As
+Begin
+	Insert Into Cursos (IDNivel, Nombre, Costocurso, CostoCertificacion, Estreno, DebeSerMayorDeEdad) Values(@IDNivel, @Nombre, @Costocurso, @CostoCertificacion, @Estreno, @DebeSerMayorDeEdad)
+End
+
+Exec SP_NuevoCurso 2, 'Aprender SQL', 2000, 1500, '2024-03-03', 0
+
+-- 8.Hacer un procedimiento almacenado que permita modificar porcentualmente el Costo de Cursada de todos los cursos. El procedimiento debe recibir un valor numérico que representa el valor a aumentar porcentualmente. Por ejemplo, si recibe un 60. Debe aumentar un 60% todos los costos.
+
+Create Or Alter Procedure SP_CostoCursada(
+@Aumento Int
+)
+As
 Begin
 
-	Select
+	Update Cursos Set CostoCurso = CostoCurso * (1 + (@Aumento / 100.0));
 
 End
+
+Exec SP_CostoCursada 20
+
+-- 9.Hacer un procedimiento almacenado llamado SP_PagarInscripcion que a partir de un IDInscripcion permita hacer un pago de la inscripción. El pago puede ser menor al costo de la inscripción o bien el total del mismo. El sistema no debe permitir que el usuario pueda abonar más dinero para una inscripción que el costo de la misma. Se debe registrar en la tabla de pagos con la información que corresponda.
+
+Create Or Alter Procedure SP_PagarInscripcion(
+@IDInscripcion Int,
+@Monto Money
+)
+As
+Begin
+
+	Declare @CostoInscripcion Money
+	Declare @MontoPago Money
+	Declare @MontoRestante Money
+	
+	Select @CostoInscripcion = I.Costo From Inscripciones I Where I.ID = @IDInscripcion
+
+	Select @MontoPago = P.Importe From Pagos P Where P.IDInscripcion = @IDInscripcion
+
+	Set @MontoRestante = @CostoInscripcion - @MontoPago
+
+	
+	If @Monto > @MontoRestante
+	Begin
+		RAISERROR('El Monto sobrepasa el costo total de la inscripcion.', 16, 0)
+		Return
+	End
+
+	Insert Pagos (IDInscripcion, Fecha, Importe) Values (@IDInscripcion, GetDate(), @Monto)
+
+End
+
+Exec SP_PagarInscripcion 31, 2000
+
+-- 10.Hacer un procedimiento almacenado llamado SP_InscribirUsuario que reciba un IDUsuario y un IDCurso y realice la inscripción a dicho curso de ese usuario. El procedimiento debe realizar las siguientes comprobaciones:
+-- El usuario no debe registrar deuda para poder inscribirse.
+-- El usuario debe ser mayor de edad si el curso requiere esa condición.
+-- En caso que estas comprobaciones sean satisfechas entonces registrar la inscripción. Determinar el costo de la inscripción al valor actual del curso. Si alguna comprobación no se cumple, indicarlo con un mensaje de error correspondiente.
+
+Create Or Alter Procedure SP_InscribiUsuario(
+@IDUsuario Int,
+@IDCurso Int
+)
+As
+Begin
+
+	Declare @Deuda Money
+	Declare @Edad Int
+	Declare @CostoCurso Money
+	Declare @DebeSerMayor Bit
+
+	Set @Deuda = dbo.FN_DeudaxUsuario(@IDUsuario)
+
+	Set @Edad = dbo.FN_CalcularEdad(@IDUsuario)
+
+	Select @CostoCurso = C.CostoCurso From Cursos C Where C.ID = @IDCurso
+
+	Select @DebeSerMayor = C.DebeSerMayorDeEdad From Cursos C Where C.ID = @IDCurso
+
+	If @Deuda != 0 Begin
+	Raiserror('El usuario presenta deuda vigente.', 16, 0)
+	Return
+	End
+
+	If @DebeSerMayor = 1 Begin
+
+			If @Edad >= 18 Begin
+				
+				Insert Into Inscripciones (IDUsuario, IDCurso, Costo) Values (@IDUsuario, @IDCurso, @CostoCurso)
+				Return
+			End
+			Else Begin
+			Raiserror('El usuario es menor a 18 años.', 16, 0)
+			Return
+			End
+	End
+
+	Insert Into Inscripciones (IDUsuario, IDCurso, Costo) Values (@IDUsuario, @IDCurso, @CostoCurso)
+End
+
+Exec SP_InscribiUsuario 23, 8
